ed5898179768c2d7884a5b9373344155d223f7d0
waldirmarques <waldirmarquesdesouza@gmail.com>
2019-06-11 22:15:57 -0300

    version 1.0

---
+++ Myproject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/waldir/Desktop/Apreendendo_site/Myproject/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ site_apreendendo/site_apreendendo/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def paginaMain(request):
+    return HttpResponse("Estou tentando novamente...")
