a55c03841c54c0f8e59ab9dad29136b7c4aee161
waldirmarques <waldirmarquesdesouza@gmail.com>
2019-06-11 23:03:51 -0300

    version 1.0

--- Myproject/bin/activate.fish
+++ Myproject/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/waldir/Desktop/Apreendendo_site/Myproject"
+set -gx VIRTUAL_ENV "/home/waldir/Desktop/siteApreendendo/Myproject"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
