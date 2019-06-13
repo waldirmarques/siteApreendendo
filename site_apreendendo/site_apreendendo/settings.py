451b99e757faccb96bb5eda6d6e9807fcfeaf9d9
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-06-13 20:40:44 -0300

    Added static variables to work whit templates

--- site_apreendendo/site_apreendendo/settings.py
+++ site_apreendendo/site_apreendendo/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    "static"
-]
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
